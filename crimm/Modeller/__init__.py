238fed7fee2bc33837a18f2ded1b96fa038b0214
StressedChemist <115736244+StressedChemist@users.noreply.github.com>
2026-06-01 16:14:02 -0400

    Import MembraneBuilder and related classes

--- crimm/Modeller/__init__.py
+++ crimm/Modeller/__init__.py
@@ -1,2 +1,3 @@
 from crimm.Modeller.TopoFixer import ResidueFixer
 from crimm.Modeller.TopoLoader import ResidueTopologySet, TopologyGenerator, ParameterLoader
+from crimm.Modeller.MembraneBuilder import MembraneBuilder, MembraneSpec, build_membrane_system
