e7345049421e124455be8fe683c82cada50d8d4c
StressedChemist <115736244+StressedChemist@users.noreply.github.com>
2026-06-06 18:07:36 -0400

    Refactor imports in __init__.py for Membrane module

--- crimm/Modeller/__init__.py
+++ crimm/Modeller/__init__.py
@@ -1,3 +1,10 @@
 from crimm.Modeller.TopoFixer import ResidueFixer
 from crimm.Modeller.TopoLoader import ResidueTopologySet, TopologyGenerator, ParameterLoader
-from crimm.Modeller.MembraneBuilder import MembraneBuilder, MembraneSpec, build_membrane_system
+from crimm.Modeller.MembraneBuilder import (
+    MembraneBuilder,
+    MembraneSpec,
+    build_membrane_system,
+    apply_charmm_gui_orientation,
+    read_charmm_gui_orientation,
+)
+from crimm.Modeller.MembraneOrienter import MembraneOrienter, MembraneOrientation
